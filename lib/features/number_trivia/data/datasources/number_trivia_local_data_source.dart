ffac023b2151450d2a1dba525500b59b3538d170
Artem Rebryk <artemrebrik8099@gmail.com>
2019-10-04 16:20:03 +0300

    make test and create fixture

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,4 +1,7 @@
-import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   /// Gets the cached [NumberTriviaModel] which was gotten the last time
@@ -8,4 +11,22 @@ abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({ @required this.sharedPreferences });
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // TODO: implement getLastNumberTrivia
+    return null;
+  }
+  
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    // TODO: implement cacheNumberTrivia
+    return null;
+  }
 }
